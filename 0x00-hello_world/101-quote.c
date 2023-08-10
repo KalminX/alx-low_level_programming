e63d1bea55e499cfab38386a37dffb1723d27f44
KalminX <noriabure@gmail.com>
2023-08-10 10:14:18 -0700

    great

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - writes to standard error
+ *
+ * Return: 1 if successful
+ *
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+	return (1);
+}
