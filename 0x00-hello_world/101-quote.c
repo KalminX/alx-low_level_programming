ebc136413ff2335afcb011843434dec05c4da9f2
KalminX <noriabure@gmail.com>
2023-08-10 10:29:00 -0700

    yh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,11 @@
  */
 int main(void)
 {
-	char message[]
+	char message[];
+
 	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, message, strlen(message));
+
 	return (1);
 }
