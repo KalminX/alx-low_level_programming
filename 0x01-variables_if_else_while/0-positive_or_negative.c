5143160738a9590529c3a086ccd02a755f9348d5
KalminX <noriabure@gmail.com>
2023-08-11 05:08:39 -0700

    yh

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+  
+ /**
+  * main - check for sign of a number
+  *
+  * Return: 0 if successful
+  *
+  */ 
+int main(void) 
+{
+	int n; 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else
+	{
+		printf("%d is positive", n);
+	}
+	return (0);
+}
