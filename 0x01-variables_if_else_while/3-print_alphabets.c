bc60a11b0d10d16cad02fef4aaa5f1c53172d188
KalminX <noriabure@gmail.com>
2023-08-12 03:41:09 -0700

    yh

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the lowercase followed by uppercase
+ *
+ * Return: 0 if successful
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A', c <= 'Z', c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
