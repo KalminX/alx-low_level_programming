3770a62f2e7944b8a8d9e2a858bb32f499afa720
KalminX <noriabure@gmail.com>
2023-08-12 05:15:35 -0700

    yh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char c;
+	
+	putchar('0')
+	for (c = '1'; c <= '9'; c++)
+	{
+		putchar(',');
+		putchar(' ');
+		putchar(c)
+	}
+
+	return (0);
+}
