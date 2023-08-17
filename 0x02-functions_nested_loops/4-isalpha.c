47b07cdfec2aa05aff88158ff0517a155b1f744d
KalminX <noriabure@gmail.com>
2023-08-17 06:23:38 -0700

    hello

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks if the character is a letter
+ *
+ * @c: parameter to be checked
+ *
+ * return: 1 if it's a letter 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
