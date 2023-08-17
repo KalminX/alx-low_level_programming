ebc411f80ca3688834a366386a5b0ed134505cf5
KalminX <noriabure@gmail.com>
2023-08-17 15:52:25 -0700

    hello

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,12 @@ void print_to_98(int n)
 {
 	int i, j;
 
-	if (n >= 98)
+	if (n == 98)
+	{
+		printf("%d", 98);
+	}
+
+	if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
@@ -24,7 +29,7 @@ void print_to_98(int n)
 		printf("\n");
 	}
 
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (j = n; j <= 98; j++)
 		{
