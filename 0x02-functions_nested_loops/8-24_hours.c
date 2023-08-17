fd6ff6328f557ed796ec33cc3329464d55875bf6
KalminX <noriabure@gmail.com>
2023-08-17 11:17:27 -0700

    hello

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		j = 0;
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 24) + '0');
+			_putchar((i % 24) + '0');
+			_putchar(':');
+			_putchar((j / 60) + '0');
+			_putchar((j % 60) + '0');
+		}
+		_putchar('\n');
+	}
+}
