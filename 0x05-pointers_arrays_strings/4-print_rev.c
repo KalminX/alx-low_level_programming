9a4c3782d4e69c1f73db8877307bf2386acf5b4d
KalminX <noriabure@gmail.com>
2023-08-21 14:29:16 -0700

    hello

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,14 @@ void print_rev(char *s)
 {
 	int len = 0, i;
 
-	while (*(str + len) != '\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
 	}
 
 	for (i = len + 1; i >= 0; i--)
 	{
-		_putchar(*(str + i));
+		_putchar(*(s + i));
 	}
 
 	_putchar('\n');
