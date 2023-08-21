e37087229cb5871e4376d97aa86ddbfa915a84b7
KalminX <noriabure@gmail.com>
2023-08-21 15:59:49 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of the string
+ * @str: the string to be printed
+ */
+void puts_half(char *str)
+{
+	int len, j, k, n;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	n = len / 2;
+
+	if (len % 2 == 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar(*(str + n + j));
+		}
+	}
+	else
+	{
+		for (k = 0; k < n; k++)
+		{
+			_putchar(*(str + n + 1 + k));
+		}
+	}
+	_putchar('\n');
+}
