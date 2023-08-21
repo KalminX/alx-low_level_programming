cadb3b2863e585e19300ac3b0fd0b33eb4953a8d
KalminX <noriabure@gmail.com>
2023-08-21 15:00:36 -0700

    hello

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,17 @@ void rev_string(char *s)
 {
 	int len = 0, i = 0, k;
 
-	while (*(s + i) != '\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
 	}
 	k = 0;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = len - 1; i > k; i--)
 	{
-		*(s + i) = *(s + k);
+		int h = *(s + k);
+		*(s + k) = *(s + i);
+		*(s + i) = h;
 		k++;
 	}
 	_putchar('\n');
