2d80837b444f791e320c16a46ac9432497984ce2
KalminX <noriabure@gmail.com>
2023-08-21 03:47:01 -0700

    hello

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,12 @@
 /**
  * _strlen - calculates the length of a string
  * @s: the string whose length is to be calculated
+ * Return: returns the length of the string
  */
 int _strlen(char *s)
 {
 	int i;
-	
+
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		_putchar(*(s + 1);
