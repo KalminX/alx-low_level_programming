4d30cfad3e8f59d271feaab220db5ace387e43ed
KalminX <noriabure@gmail.com>
2023-08-21 03:55:04 -0700

    hello

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,13 @@
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	while (*(s + i) != '\0;)
 	{
-		_putchar(*(s + 1);
+		i++;
 	}
 
 
-	return (i + 1);
+	return (i);
 }
