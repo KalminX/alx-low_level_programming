ed00f31b110b3bb02a7295d8abe2d89cb38023ad
KalminX <noriabure@gmail.com>
2023-08-23 20:20:22 -0700

    yh

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,6 +3,7 @@
  * _strcmp - a function that compares two strings
  * @s1: the first string
  * @s2: the second string
+ * Return: returns the difference between between their ascii values
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,9 +17,5 @@ int _strcmp(char *s1, char *s2)
 
 	if (*s1 == *s2)
 		return (0);
-	else if (*s1 < *s2)
-	{
-		return (-1);
-	}
-	return (1);
+	return (*s1 - *s2);
 }
