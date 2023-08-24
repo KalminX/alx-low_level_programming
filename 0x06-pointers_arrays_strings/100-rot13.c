822f37f0829418cd74f52a847863806defa81710
KalminX <noriabure@gmail.com>
2023-08-24 02:47:13 -0700

    I rule

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - replaces a letter array with the letter 13 characters in front of it
+ * @str: the letter array to be modified
+ * Return: returns a pointer to the string
+ */
+char *rot13(char *str)
+{
+	int i;
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*str != '\0')
+	{
+		for (i = 0; i < 26; i++)
+		{
+			if (*str == *input)
+				*str = *output;
+		}
+		*str++;
+	}
+	return (str);
+}
