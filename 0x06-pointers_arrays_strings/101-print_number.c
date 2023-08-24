f67caaa3bca119ee103f32b2393431097dc0f96a
KalminX <noriabure@gmail.com>
2023-08-24 13:50:53 -0700

    hello

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - a function that
+ * prints an integer without using the standard library
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+	int digit, div = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	while (n / div >= 10)
+	{
+		div *= 10;
+	}
+
+	while (div > 0)
+	{
+		digit /= div;
+		_putchar('0' + digit);
+		digit %= div;
+		div /= 10;
+	}
+}
