189508264e22840d5e0826ec01a7401b75083e05
KalminX <noriabure@gmail.com>
2023-08-31 15:42:04 -0700

    creater a power function with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _pow_recursion - replicating the power function using recursion
+ * @x: the base number
+ * @y: the exponent
+ * Return: returns the result of the exponent
+ */
+int _pow_recursion(int x, int y)
+{
+	int _pow(int x, int y)
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (y == 1)
+		{
+			return (x);
+		}
+		else if (y == 0)
+		{
+			return (1);
+		}
+		else if (y > 1)
+		{
+			return x * _pow(x, (y -1));
+		}
+	}
+}
