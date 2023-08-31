24ee1a7668dc7aff2a24be118061a9e214678d3e
KalminX <noriabure@gmail.com>
2023-08-31 16:03:11 -0700

    lets go yh

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,19 +11,16 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else
+	if (y == 1)
 	{
-		if (y == 1)
-		{
-			return (x);
-		}
-		else if (y == 0)
-		{
-			return (1);
-		}
-		else if (y > 1)
-		{
-			return (x * _pow_recursion(x, (y - 1)));
-		}
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y > 1)
+	{
+		return (x * _pow_recursion(x, (y - 1)));
 	}
 }
