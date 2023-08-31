bef0205aec0f230619c1b15edc12d1bacdc3b82a
KalminX <noriabure@gmail.com>
2023-08-31 02:51:43 -0700

    hello

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,12 +10,12 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else 
+	else
 	{
 		if (n == 0)
 		{
 			return (1);
 		}
-		return (n *(factorial(n - 1)));
+		return (n * (factorial(n - 1)));
 	}
 }
