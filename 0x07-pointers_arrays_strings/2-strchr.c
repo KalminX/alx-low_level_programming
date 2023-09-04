b5921b2f35bd6d0ca27daeac200ef6c2092eb8e1
KalminX <noriabure@gmail.com>
2023-09-04 13:03:31 -0700

    created a function that replicates the function of strchr in C

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strchr - a function that returns the pointer to the
+ * first occurrence of a character
+ * @s: the string to be checked
+ * @c: the character whose pointer is to be returned
+ * Return: returns a pointer to the string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		s++;
+	return (s);
+}
