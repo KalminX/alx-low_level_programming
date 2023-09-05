7a99bd8d18e01559ef12c34b626d8926a660beb9
KalminX <noriabure@gmail.com>
2023-09-04 18:59:58 -0700

    just me learning how to use command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * nameis - prints the first argument
+ * @argc: argument count
+ * @argv: argument vector
+ */
+void nameis(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
