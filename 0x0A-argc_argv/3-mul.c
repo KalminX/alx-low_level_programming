6a6d00d4ff709d4fcd9ba97c177e7a814f02f9fb
KalminX <noriabure@gmail.com>
2023-09-04 19:52:46 -0700

    created a function that multiplies the arguments supplied to the program

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiplies the arguments given to the command line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc == 1)
+		printf("Error\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+		printf("%d", product);
+	}
+}
