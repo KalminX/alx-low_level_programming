bb95f35f5752dfb6c253ea451ebc1b45cce29426
KalminX <noriabure@gmail.com>
2023-09-08 18:36:15 -0700

    created a function that creates an array using memory from the heap

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- *
- *
- *
- *
+ * create_array - creates an array using
+ * memory from the heap
+ * @size: the size in bytes allocated
+ * @c: the pointer to the allocated
+ * memory
+ * Return: returns NULL if size is 0
+ * and returns the pointer if successful
  */
 char *create_array(unsigned int size, char c)
 {
