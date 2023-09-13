c89e7d456a30c28180b2dcbcfb64f7861caf47a1
KalminX <noriabure@gmail.com>
2023-09-13 13:49:44 -0700

    Created a struct for dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a struct for any dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /*MAIN_H*/
