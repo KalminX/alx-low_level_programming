42d3ef48e515c5f599cd9ded63097e9fe155afab
KalminX <noriabure@gmail.com>
2023-09-17 03:50:38 -0700

    Created a function that allocates a gris

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * alloc_grid - a function that allocates memory for a grid
+ * @width: the columns
+ * @height: the rows
+ * Return: returns a pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int *h, *w, i = 0, j = 0;
+
+	h = (int **) malloc(height * sizeof(int));
+	if (h == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		w = (int *) malloc(width * sizeof(int));
+		if (w == NULL)
+			return (NULL);
+		*(h + i) = w;
+	}
+	return (h);
+}
