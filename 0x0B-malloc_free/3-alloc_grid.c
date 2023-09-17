ce6f6ad7e6b4385ff5391ff1efffdbe78e786dfe
KalminX <noriabure@gmail.com>
2023-09-17 04:52:17 -0700

    Created a function that allocates a grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,9 @@ int **alloc_grid(int width, int height)
 		h[i] = (int *) malloc(width * sizeof(int));
 		if (h[i] == NULL)
 		{
-			free(h[i]);
+			for (j = 0; j < i; j++)
+				free(h[j]);
+			free(h);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
