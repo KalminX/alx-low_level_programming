797720049e2b0872219f18f84d3a1b773950738d
KalminX <noriabure@gmail.com>
2023-09-22 15:21:44 -0700

    created a function that adds a node to the end of a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,32 +1,24 @@
-#include <stdio.h>
 #include "lists.h"
 /**
- * add_node - adds a node tye tye beginnig of
- * linked list
- * @head: the head of the list
- * @str: the string in the node
- * Return: returns the new node
+ *
+ *
+ *
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	char *n_str;
-
-	if (str == NULL)
-		return (NULL);
+	list_t new_node;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	if (new_node != NULL)
 	{
-		free(new_node->str);
-		return (NULL);
+		new_node->len = strlen(str);
+		new_node->next = *head;
+		new_node->str = strdup(str);
 	}
-	new_node->len = strlen(str);
-	new_node->next = *head;
+	else
+		return(NULL);
+	if (*head != NULL)
+		new_node->next = *head;
 	*head = new_node;
-
 	return (new_node);
 }
