0f17bea2d041db0e5d19e0d9a85094c02bdaa66a
KalminX <noriabure@gmail.com>
2023-09-22 18:11:46 -0700

    created a function that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * myStartupFun - a function that executes before main
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
