966c5bfa65f182f034d09c496633ea7d0941fb66
KalminX <noriabure@gmail.com>
2023-09-24 11:07:42 -0700

    created a function that prints strings entered as arguments no matter the data type

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+/**
+ * print_all - a function that can print anything
+ * @format: a string containing the specifiers
+ */
+void print_all(const char * const format, ...)
+{
+	va_list my_entries;
+
+	int i = 0;
+	char *ch = "";
+
+	va_start(my_entries, format);
+	while (*(format + i))
+	{
+		switch (*(format + i))
+		{
+		case 'f':
+			printf("%s%f", ch, va_arg(my_entries, float));
+			break;
+		case 'i':
+			printf("%s%d", ch, va_arg(my_entries, int));
+			break;
+		case 'c':
+			printf("%s%c", ch, va_arg(my_entries, int));
+			break;
+		case 's':
+			if (va_arg(my_entries, char*))
+				printf("%s%s", ch, va_arg(my_entries, char*));
+			else
+				printf("%s%s", ch, "(nil)");
+		default:
+			break;
+		}
+		i++;
+		first = 0;
+		ch = ", ";
+	}
+	va_end(my_entries);
+	printf("\n");
+}
