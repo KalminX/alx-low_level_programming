1c657f746b25c08aeb7bc995a7addb6481379a4b
KalminX <noriabure@gmail.com>
2023-09-25 02:43:31 -0700

    created a function that prints the elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints the data in a linked list
+ * @h: the head of the list
+ * Return: the number of elements in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
