f99e0c8ce7996f0a774d8b32493a47e3cf4e88a7
KalminX <noriabure@gmail.com>
2023-09-25 11:26:48 -0700

    created a function that checks for proberly allocated memory

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_nconcat - the function
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the first bytes of s2
+ * Return: return a pointer to char
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k = 0;
+	char *ptr;
+
+	ptr = malloc(strlen(s1) + n + 1);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (*(s1 + i))
+	{
+		*(ptr + i) = *(s1 + i);
+		i++;
+	}
+	j = 0;
+	while (k < n && s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+		k++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
