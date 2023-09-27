6ca14fcdb9cee70f5496ebf6b37d587cd4b32738
KalminX <noriabure@gmail.com>
2023-09-27 00:00:49 -0700

    created a function that allocates memory for a range of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc((dif + 1) * sizeof(int));
+	ptr = malloc((diff + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
 
