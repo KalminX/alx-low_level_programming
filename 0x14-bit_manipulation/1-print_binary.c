10936ecef690058d06d6a00f22ad8ca5d7d3afa6
KalminX <noriabure@gmail.com>
2023-09-29 16:47:39 -0700

    a function that prints the binary of a decimal number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary equivalent
+ * of a decimal
+ * @n: the decimal
+ */
+void print_binary(unsigned long int n)
+{
+	int i, is_one = 0;
+
+	if (n == 0)
+	{
+		printf("0");
+	}
+
+	for (i = 31; i >= 0; i--)
+	{
+		if (n & (1 << i))
+		{
+			is_one = 1;
+			printf("1");
+		}
+		if (is_one && !(n & (1 << i)))
+		{
+			printf("0");
+		}
+	}
+}
