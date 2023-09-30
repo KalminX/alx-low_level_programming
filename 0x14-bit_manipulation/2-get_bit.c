bb84d685e2ddf727174be1f3cc7086a350b50188
KalminX <noriabure@gmail.com>
2023-09-30 02:50:25 -0700

    created a function that checks if the bit is set or not

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,12 +3,15 @@
  * get_bit - checks if the bit of a numher at
  * a particular index is set or not
  * @n: the number
- * @i: the index
+ * @index: the index
  * Return: returns 0 or 1 if its either
  * and -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (n == 0)
+		return (0);
+
 	if ((n >> index) == 0)
 		return (-1);
 
