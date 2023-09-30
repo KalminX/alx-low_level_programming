1e61be14947ca4bbddf209db1128cb5ef550a36a
KalminX <noriabure@gmail.com>
2023-09-30 03:16:37 -0700

    fixed the 2-print file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,13 +9,15 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int long_1 = 1;
+
 	if (n == 0)
 		return (0);
 
-	if ((n >> index) == 0)
+	if (index >= 63)
 		return (-1);
 
-	if (n & (1 << index))
+	if (n & (long_1 << index))
 		return (1);
 	else
 		return (0);
