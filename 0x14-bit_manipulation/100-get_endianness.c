199ccc3e342a1e43d48b6a80be731f0f1a9aa827
KalminX <noriabure@gmail.com>
2023-10-01 05:39:55 -0700

    a function that checks the endianess of a computer architecture

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - determines the endianness
+ * of a computer's architecture
+ * Return: returns 1 if it's big endian and 0
+ * if it's little endian
+ */
+int get_endianness(void)
+{
+	int x = 1;
+	char *c = (char*) &x;
+
+	return(*c);
+}
