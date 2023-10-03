0f28ab1be57a49bf189abdfd2cb2880d9653a3dc
KalminX <noriabure@gmail.com>
2023-10-03 12:04:00 -0700

    fixed the part that handles empty string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (fp == -1)
 		return (-1);
 
-	if (text != NULL)
+	if (text_content != NULL)
 		write(fp, text_content, strlen(text_content));
 	close(fp);
 
