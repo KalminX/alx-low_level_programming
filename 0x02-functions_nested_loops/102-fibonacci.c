2821dd678b19e232ad632083f2aa42f94038a0b6
KalminX <noriabure@gmail.com>
2024-12-08 01:10:42 +0100

    solved the second advanced task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a function that prints out the fibonacci
+ * sequence.
+ * Return: Returns 0 if successful
+ */
+int main(void)
+{
+    long int i, sum, sum_before, copy;
+
+    printf("%d, %d, ", 1, 2);
+    sum_before = 2;
+    sum = 3;
+    for (i = 2; i <= 50; i++)
+    {
+        if (i < 50)
+        {
+            printf("%li, ", sum);
+            copy = sum;
+            sum += sum_before;
+            sum_before = copy;
+        }
+        else if(i == 50)
+        {
+            printf("%li\n", sum + sum_before);
+        }
+    }
+    return (0);
+}
