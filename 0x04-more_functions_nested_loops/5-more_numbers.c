9aa906df023d0bcfe952fe9993a031fc9bead92e
KalminX <noriabure@gmail.com>
2024-12-08 20:26:50 +0100

    solved the sixth task on  more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - A function that prints numbers 0-14 ten times.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j >= 10)
+			{
+				_putchar('0' + (j / 10));
+			}
+			_putchar('0' + (j % 10));
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
