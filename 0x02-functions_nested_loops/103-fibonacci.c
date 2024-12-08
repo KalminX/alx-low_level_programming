f08beae844c551583d6ac206fda6b54b2a91257a
KalminX <noriabure@gmail.com>
2024-12-08 07:34:51 +0100

    conformed my code to betty styling

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,12 +3,12 @@
 
 /**
  * main - a function that prints out the fibonacci
- * sequence.
+ * sequence that's less than 4000000.
  * Return: Returns 0 if successful
  */
 int main(void)
 {
-	long int i, sum, sum_before, copy;
+	long int sum, sum_before, copy;
 
 	printf("%d, %d, ", 1, 2);
 	sum_before = 2;
