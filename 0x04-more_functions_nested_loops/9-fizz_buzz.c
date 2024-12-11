4af4ce2919dcb69d7b2dcd10777a5f035dee487e
KalminX <noriabure@gmail.com>
2024-12-11 23:06:29 +0100

    solved the fizzbuzz task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,34 +2,34 @@
 #include <stdio.h>
 
 /**
+ * main - A function that solves the fizzbuzz challenge
  *
- *
- *
+ * Return: It returns 0 if successful
  */
- int main(void)
- {
-    int i;
+int main(void)
+{
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 5 == 0 && i % 3 == 0)
-        {
-            printf(" %s", "FizzBuzz");
-        }
-        else if(i % 3 == 0)
-        {
-            printf(" %s", "Fizz");
-        }
-        else if(i % 5 == 0)
-        {
-            printf(" %s", "Buzz");
-        }
-        else
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf(" %s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" %s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" %s", "Buzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
 
-    return (0);
- }
+	return (0);
+}
